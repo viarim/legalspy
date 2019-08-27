5df3660a0bdb4fd4690f450831094b92374462ff
jevgenij <djtester@gmail.com>
2019-08-27 14:35:42 +0300

    Fixed SecurityConfig

--- src/main/java/com/accenture/bootcamp/legalspy/config/SecurityConfig.java
+++ src/main/java/com/accenture/bootcamp/legalspy/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.accenture.bootcamp.legalspy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+		    web
+		        .ignoring()	      
+		        .antMatchers("/bootstrap/**");
+		}
 	
 	    @Override
 	    protected void configure(HttpSecurity http) throws Exception {
