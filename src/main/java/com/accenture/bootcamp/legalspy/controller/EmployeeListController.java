834eb3a5260fdbdd6e3ce2e2b64b8508f22abf70
ilsssa <iiss@inbox.lv>
2019-08-28 18:02:25 +0300

    something works !

--- src/main/java/com/accenture/bootcamp/legalspy/controller/EmployeeListController.java
+++ src/main/java/com/accenture/bootcamp/legalspy/controller/EmployeeListController.java
@@ -1,6 +1,10 @@
 package com.accenture.bootcamp.legalspy.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import com.accenture.bootcamp.legalspy.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +16,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class EmployeeListController {
 
 	@RequestMapping(value = { "/employeeList" }, method = RequestMethod.GET)
-	public String employeeList(Model model) {
-
+	public String employeeList(Model model) throws SQLException {
+				
+		EmployeeManager e = new EmployeeManager();
+		List<Employee> emps = e.findEmployees();
+		
+		model.addAttribute("employees", emps);
+			
+		
+		
 		return "employeeList";
 
 	}
 
-		@RequestMapping(value = { "/feedbackForm" }, method = RequestMethod.GET)
-		public String feedbackForm(Model model) {
-
-			return "feedbackForm";
-
-	}
+	
 	
 }
