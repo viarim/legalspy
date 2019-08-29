1d6fa219cc703331b5fe1d71b6e173538dc11f83
ilsssa <iiss@inbox.lv>
2019-08-29 20:39:47 +0300

    feedback form name updated

--- src/main/java/com/accenture/bootcamp/legalspy/controller/EmployeeListController.java
+++ src/main/java/com/accenture/bootcamp/legalspy/controller/EmployeeListController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,11 +30,4 @@ public class EmployeeListController {
 
 	}
 
-
-	@RequestMapping(value = { "/feedbackForm" }, method = RequestMethod.GET)
-	public String feedbackForm(Model model) {
-
-		return "feedbackForm";
-	}
-	
 }
